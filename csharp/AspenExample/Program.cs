d9dcf853c1803df6da88fb7f35a328175477f4bf
Luke Bayes <lukebayes@gmail.com>
2020-07-13 17:52:03 -0400

    Added user facing text to sample project

--- csharp/AspenExample/Program.cs
+++ csharp/AspenExample/Program.cs
@@ -14,19 +14,30 @@ namespace AspenExample
                 Version version = aspen.GetFirmwareVersion(path);
                 Console.WriteLine("A firmware update (version {0}) is available.", version.ToString());
                 Console.WriteLine("The computer must remain plugged in and left alone during firmware updates.");
-                Console.WriteLine("Would you like to update now? [Y/n]");
-                string response = Console.ReadLine();
-                if (response == "" || response.ToUpper() == "Y")
+                Console.WriteLine("Would you like to update now? [Y/n] (Press Enter for Yes)");
+                string userResponse = Console.ReadLine();
+                if (userResponse == "" || userResponse.ToUpper() == "Y")
                 {
-                    aspen.UpdateFirmware(path);
+                    Console.WriteLine("Thank you, attempting to update firmware now.");
+                    // TODO(lbayes): Subscribe to progress notifications.
+                    DfuResponse response = aspen.UpdateFirmware(path);
+                    if (response == DfuResponse.SUCCESS)
+                    {
+                        Console.WriteLine("Firmware update completed successfully.");
+                    }
+                    else
+                    {
+                        Console.Write("Firmware update failed with error code {0}", response);
+                    }
                 }
                 else
                 {
                     Console.WriteLine("Deferring update for now, thank you!");
                 }
-            } else
+            }
+            else
             {
-                Console.WriteLine("Your Aspen firmware is up to date.");
+                Console.WriteLine("Your Aspen firmware is already up to date.");
             }
         }
     }
