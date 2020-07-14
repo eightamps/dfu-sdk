da847f564060c8eaa887825d6637c4b50aaf8ff0
Luke Bayes <lukebayes@gmail.com>
2020-07-14 15:16:39 -0400

    Cleaning up implementation

--- csharp/Aspen/Aspen.cs
+++ csharp/Aspen/Aspen.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Text.RegularExpressions;
 using DeviceProgramming.Dfu;
 using DeviceProgramming.FileFormat;
@@ -10,7 +11,9 @@ namespace EightAmps
         SUCCESS = 0,
         VERSION_IGNORED,
         TIMEOUT = 10,
-        NOT_DFU_FILE = 50,
+        FILE_NOT_VALID = 50,
+        FILE_NOT_FOUND,
+        NEWER_VERSION,
         CONNECTION_FAILURE,
         UNKNOWN_FAILURE,
     }
@@ -51,11 +54,25 @@ namespace EightAmps
             return new Version(major, minor);
         }
 
+        /**
+         * Interrogate the connected USB device and find out what version it
+         * was flashed with.
+         */
+        private Version GetConnectedAspenVersion()
+        {
+            // TODO(lbayes): Interrogate the connected device for version info.
+            return new Version(0x00, 0x00);
+        }
+
+        /**
+         * Get the version information from the provided DFU file. If the file
+         * is invalid, return zeros for the version information.
+         */
         public Version GetFirmwareVersionFromDfu(string dfuFilePath)
         {
             if (!isDfuFile(dfuFilePath))
             {
-                return new Version(0xFF, 0xFF);
+                return new Version(0x00, 0x00);
             }
 
             return GetVersionFromFileName(dfuFilePath);
@@ -63,20 +80,49 @@ namespace EightAmps
             // return Dfu.ParseFile(dfuFilePath).DeviceInfo.ProductVersion;
         }
 
-        public bool ShouldUpdateFirmware(string dfuFilePath)
+        /**
+         * Check all appropriate values to ensure an update can and should take
+         * place. Return a helpful status code if a firmware update is not
+         * appropriate at this time.
+         */
+        private DfuResponse shouldUpdateFirmware(string dfuFilePath)
         {
+            if (!File.Exists(dfuFilePath))
+            {
+                return DfuResponse.FILE_NOT_FOUND;
+            }
             if (!isDfuFile(dfuFilePath))
             {
-                return false;
+                return DfuResponse.FILE_NOT_VALID;
             }
-            return true;
+            if (GetConnectedAspenVersion() >= GetFirmwareVersionFromDfu(dfuFilePath))
+            {
+                return DfuResponse.NEWER_VERSION;
+            }
+            return DfuResponse.SUCCESS;
         }
 
+        /**
+         * Return true if all appropriate values confirm that an update can and
+         * should take place. Return a helpful status code if a firmware update
+         * is not appropriate at this time.
+         */
+        public bool ShouldUpdateFirmware(string dfuFilePath)
+        {
+            return shouldUpdateFirmware(dfuFilePath) == DfuResponse.SUCCESS;
+        }
+
+        /**
+         * Perform an update after verifying that it should be done. If an
+         * update is not appropriate or does not success, return a helpful
+         * status code.
+         */
         public DfuResponse UpdateFirmware(string dfuFilePath)
         {
-            if (!isDfuFile(dfuFilePath))
+            var shouldUpdate = shouldUpdateFirmware(dfuFilePath);
+            if (shouldUpdate != DfuResponse.SUCCESS)
             {
-                return DfuResponse.NOT_DFU_FILE;
+                return shouldUpdate;
             }
 
             return DfuResponse.SUCCESS;
