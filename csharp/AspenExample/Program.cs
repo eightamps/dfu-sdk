952014d414ebcc95e88ba84a1889e842effc5cdb
Luke Bayes <lukebayes@gmail.com>
2020-07-15 15:45:43 -0400

    Added console UI to example project

--- csharp/AspenExample/Program.cs
+++ csharp/AspenExample/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Net;
+using System.Threading;
 using EightAmps;
 
 namespace AspenExample
@@ -9,64 +10,101 @@ namespace AspenExample
     {
         static void Main(string[] args)
         {
-            string path = "firmwares/Aspen-v1.3.dfu";
-            var shouldForceVersion = false;
-            string userResponse = "";
-            var aspen = new Aspen();
-            var shouldUpdate = aspen.ShouldUpdateFirmware(path, shouldForceVersion);
-            Version version = aspen.GetFirmwareVersionFromDfu(path);
-            Version oldVersion = aspen.GetConnectedAspenVersion();
+            while (true) {
+                // string path = "firmwares/Aspen-v1.2.dfu";
+                var shouldForceVersion = false;
+                string userResponse = "";
+                Console.WriteLine("Provide a path to a firmware update (e.g., C:\\Aspen-v1.1.dfu), or type 'q' to quit:");
+                string path = Console.ReadLine();
 
-            aspen.DownloadProgressChanged += (object sender, ProgressChangedEventArgs e) =>
-            {
-                Console.WriteLine("Download progress: {0}%", e.ProgressPercentage);
-            };
-
-            if (shouldUpdate == DfuResponse.VERSION_IS_OKAY)
-            {
-                Console.WriteLine("A firmware update version {0} is available and you have version {1} installed.",
-                    version.ToString(), oldVersion.ToString());
-                Console.WriteLine("Would you like to force the update anyway? [Y/n]");
-                userResponse = Console.ReadLine();
-                if (userResponse == "" || userResponse.ToUpper() == "Y")
+                if (path == "")
                 {
-                    shouldForceVersion = true;
+                    Console.WriteLine("Path must not be empty.");
+                    continue;
                 }
-            }
 
-            if (shouldForceVersion || shouldUpdate == DfuResponse.SHOULD_UPDATE)
-            {
-                Console.WriteLine("The computer must remain plugged in and left alone during firmware updates.");
-                if (!shouldForceVersion)
+                if (path == "q")
+                {
+                    Console.WriteLine("Exiting Now");
+                    Thread.Sleep(500);
+                    System.Environment.Exit(0);
+                }
+
+                Console.WriteLine("Performing update with {0}", path);
+
+                var aspen = new Aspen();
+                var shouldUpdate = aspen.ShouldUpdateFirmware(path, shouldForceVersion);
+                Version version = aspen.GetFirmwareVersionFromDfu(path);
+                Version oldVersion = null;
+                try
+                {
+                    oldVersion = aspen.GetConnectedAspenVersion();
+                } catch (Exception e)
+                {
+                    Console.WriteLine("No device found");
+                    Console.WriteLine(e.ToString());
+                }
+
+                aspen.DownloadProgressChanged += (object sender, ProgressChangedEventArgs e) =>
+                {
+                    Console.WriteLine("Download progress: {0}%", e.ProgressPercentage);
+                };
+
+                if (shouldUpdate == DfuResponse.VERSION_IS_OKAY)
                 {
                     Console.WriteLine("A firmware update version {0} is available and you have version {1} installed.",
                         version.ToString(), oldVersion.ToString());
-                    Console.WriteLine("Would you like to update now? [Y/n] (Press Enter for Yes)");
+                    Console.WriteLine("Would you like to force the update anyway? [Y/n]");
                     userResponse = Console.ReadLine();
+                    if (userResponse == "" || userResponse.ToUpper() == "Y")
+                    {
+                        shouldForceVersion = true;
+                    }
                 }
 
-                if (userResponse == "" || userResponse.ToUpper() == "Y")
+                if (shouldForceVersion || shouldUpdate == DfuResponse.SHOULD_UPDATE)
                 {
-                    Console.WriteLine("Thank you, attempting to update firmware now.");
-                    // TODO(lbayes): Subscribe to progress notifications.
-                    DfuResponse response = aspen.UpdateFirmware(path, shouldForceVersion);
-                    if (response == DfuResponse.SUCCESS)
+                    Console.WriteLine("The computer must remain plugged in and left alone during firmware updates.");
+                    if (!shouldForceVersion)
                     {
-                        Console.WriteLine("Firmware update completed successfully.");
+                        Console.WriteLine("A firmware update version {0} is available and you have version {1} installed.",
+                            version.ToString(), oldVersion.ToString());
+                        Console.WriteLine("Would you like to update now? [Y/n] (Press Enter for Yes)");
+                        userResponse = Console.ReadLine();
+                    }
+
+                    if (userResponse == "" || userResponse.ToUpper() == "Y")
+                    {
+                        Console.WriteLine("Thank you, attempting to update firmware now.");
+                        // TODO(lbayes): Subscribe to progress notifications.
+                        DfuResponse response = aspen.UpdateFirmware(path, shouldForceVersion);
+                        if (response == DfuResponse.SUCCESS)
+                        {
+                            Console.WriteLine("Firmware update completed successfully.");
+                        }
+                        else
+                        {
+                            Console.Write("Firmware update failed with error code {0}", response);
+                        }
                     }
                     else
                     {
-                        Console.Write("Firmware update failed with error code {0}", response);
+                        Console.WriteLine("Deferring update for now, thank you!");
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Deferring update for now, thank you!");
+                    Console.WriteLine("Your Aspen firmware could not be updated because: {0}", shouldUpdate);
                 }
-            }
-            else
-            {
-                Console.WriteLine("Your Aspen firmware could not be updated because: {0}", shouldUpdate);
+
+                Console.WriteLine("Press 'q' followed by 'enter' to exit, or just 'enter' to try again.");
+                var shouldExit = Console.ReadLine();
+                if (shouldExit == "q")
+                {
+                    Console.WriteLine("Exiting now");
+                    Thread.Sleep(500);
+                    System.Environment.Exit(0);
+                }
             }
         }
     }
