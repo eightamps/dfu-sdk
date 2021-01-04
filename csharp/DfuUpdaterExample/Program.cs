de8143eb51705d39973c2772ea07bf09846c1dd4
michael-lawrence-AutonoMe <michael.lawrence@accessibilityservices.com>
2021-01-04 10:54:01 -0500

    Allowing DFU updater example app accept command line arguments, sepcifically DFU file paths. Also, overloaded UpdateMaple method so we can either force a DFU version to latest if no command line arguments are provided or utilize the provided path.

--- csharp/DfuUpdaterExample/Program.cs
+++ csharp/DfuUpdaterExample/Program.cs
@@ -1,13 +1,24 @@
 ﻿using System;
+using System.Threading;
 using EightAmps;
 
 namespace DfuUpdaterExample
 {
     class Program
     {
-        private static readonly string MapleDfuPath = @"dfu\Maple-v3.9.dfu";
+        private static readonly string MAPLE_DFU_PATH_DEFAULT = @"dfu\Maple-v3.9.dfu";
 
         static void UpdateMaple(bool forceUpdate = false, bool breakOnStm32 = false)
+        {
+            PerformUpdate(MAPLE_DFU_PATH_DEFAULT, forceUpdate, breakOnStm32);
+        }
+
+        static void UpdateMaple(string mapleDfuPath, bool forceUpdate = false, bool breakOnStm32 = false)
+        {
+            PerformUpdate(mapleDfuPath, forceUpdate, breakOnStm32);
+        }
+
+        static void PerformUpdate(string mapleDFUPath, bool forceUpdate = false, bool breakOnStm32 = false)
         {
             try
             {
@@ -17,7 +28,7 @@ namespace DfuUpdaterExample
                 dfu.BreakOnStmBootloader = breakOnStm32; // Only used for dev troubleshooting
                 dfu.DfuProgressChanged += OnDfuProgress;
                 dfu.DfuCompleted += OnDfuCompleted;
-                DfuResponse response = dfu.UpdateMapleFirmware(MapleDfuPath, forceUpdate, true);
+                DfuResponse response = dfu.UpdateMapleFirmware(mapleDFUPath, forceUpdate, true);
                 Console.WriteLine($"Finished with {response}");
             }
             catch (Exception e)
@@ -48,41 +59,65 @@ namespace DfuUpdaterExample
             UpdateMaple(true, true);
         }
 
-        static void Main()
+        static void Main(string[] args)
         {
-            while(true)
+            if(args.Length == 0)
             {
-                Console.WriteLine("------------------------");
-                Console.WriteLine("IMPORTANT NOTE:");
-                Console.WriteLine("Ensure device (Maple or Aspen) is powered on and connected over USB before beginning.");
-                Console.WriteLine("------------------------");
-                Console.WriteLine("Press [Enter] to force upgrade Maple V3 firmware (regardless of existing version)");
-                Console.WriteLine("Press m + Enter to upgrade Maple V3 firmware only if necessary");
-                Console.WriteLine("Press s + Enter to manually enter STM BOOTLOADER mode");
-                Console.WriteLine("Press x + Enter to exit");
-                Console.WriteLine("------------------------");
-                string input = Console.ReadLine();
-                switch (input)
+                Console.WriteLine($"No arguments provided, performing force DFU update with {MAPLE_DFU_PATH_DEFAULT}");
+                UpdateMaple(true);
+            }
+            else
+            {
+                var mapleDfuFilePath = args[0];
+
+                if (string.IsNullOrEmpty(mapleDfuFilePath))
                 {
-                    case "":
-                        UpdateMaple(true);
-                        break;
-                    case "m":
-                        UpdateMaple();
-                        break;
-                    case "s":
-                        EnterStm32Bootloader();
-                        break;
-                    case "x":
-                        Console.WriteLine("------------------------");
-                        Console.WriteLine("Exiting now");
-                        return;
-                    default:
-                        Console.WriteLine("------------------------");
-                        Console.WriteLine("ERROR: Unexpected input, please try again.");
-                        break;
+                    Console.WriteLine("Maple DFU file path is invalid, please try again.");
+                    Environment.Exit(1);
+                }
+                else
+                {
+                    Console.WriteLine($"Maple DFU file path was provided: {mapleDfuFilePath}");
+                    UpdateMaple(mapleDfuFilePath, true);
                 }
             }
+
+            Thread.Sleep(5000);
+
+            //while(true)
+            //{
+            //    Console.WriteLine("------------------------");
+            //    Console.WriteLine("IMPORTANT NOTE:");
+            //    Console.WriteLine("Ensure device (Maple or Aspen) is powered on and connected over USB before beginning.");
+            //    Console.WriteLine("------------------------");
+            //    Console.WriteLine("Press [Enter] to force upgrade Maple V3 firmware (regardless of existing version)");
+            //    Console.WriteLine("Provide the DFU Update Path followed by pressing [Enter]");
+            //    //Console.WriteLine("Press m + Enter to upgrade Maple V3 firmware only if necessary");
+            //    //Console.WriteLine("Press s + Enter to manually enter STM BOOTLOADER mode");
+            //    //Console.WriteLine("Press x + Enter to exit");
+            //    //Console.WriteLine("------------------------");
+            //    string input = Console.ReadLine();
+            //    switch (input)
+            //    {
+            //        case "":
+            //            UpdateMaple(true);
+            //            break;
+            //        case "m":
+            //            UpdateMaple();
+            //            break;
+            //        case "s":
+            //            EnterStm32Bootloader();
+            //            break;
+            //        case "x":
+            //            Console.WriteLine("------------------------");
+            //            Console.WriteLine("Exiting now");
+            //            return;
+            //        default:
+            //            Console.WriteLine("------------------------");
+            //            Console.WriteLine("ERROR: Unexpected input, please try again.");
+            //            break;
+            //    }
+            //}
         }
     }
 }
